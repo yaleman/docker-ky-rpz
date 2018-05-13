bccf342177a7058ffe9d3efcc1cb576b684ea25c
yaleman <yaleman@ricetek.net>
2018-05-14 08:58:17 +1000

    gave up on proxying the stuff

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SQUIDIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' squid)
+#SQUIDIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' squid)
 
-echo Proxy at $SQUIDIP
+#echo Proxy at $SQUIDIP
 
-export http_proxy="http://$SQUIDIP:3128"
-export https_proxy="http://$SQUIDIP:3128"
+#export http_proxy="http://$SQUIDIP:3128"
+#export https_proxy="http://$SQUIDIP:3128"
 
-docker build --tag yaleman/ky-rpz --no-cache --build-arg http_proxy=http://$SQUIDIP:3128  . 
+docker build --tag yaleman/ky-rpz --no-cache . 
+#--build-arg http_proxy=http://$SQUIDIP:3128  . 
